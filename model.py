cfe036e8262d78ef94896c4aab4958981642fe8e
Ilia Krets <mrchair2170@gmail.com>
2019-07-22 17:34:03 +0200

    Initial commit

---
+++ model.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+from slim.nets.mobilenet import mobilenet_v2
+
+
+def mobilenet_backbone(input_tensor, depth_multiplier,
+                       output_stride, is_training, weight_decay):
+    with tf.contrib.slim.arg_scope(mobilenet_v2.training_scope(is_training=is_training, weight_decay=weight_decay)):
+        with tf.contrib.slim.arg_scope([tf.contrib.slim.conv2d],
+                                       normalizer_params={'scale': True, 'center': True, 'epsilon': 1e-3,
+                                                          'decay': 0.95}):
+            logits, endpoints = mobilenet_v2.mobilenet(input_tensor=input_tensor,
+                                                       num_classes=2,
+                                                       depth_multiplier=depth_multiplier,
+                                                       output_stride=output_stride,
+                                                       final_endpoint='layer_18')
+
+    net = endpoints['layer_18']
+    return net
+
+
+def load_mobilenet_weights(sess, checkpoint):
+    to_restore = [t for t in tf.all_variables() if ('Logits' not in t.name and 'Conv_1' not in t.name)]
+    saver = tf.train.Saver(var_list=to_restore, allow_empty=True)
+    saver.restore(sess, checkpoint)
+
+
+def segmentation_head(input_tensor, net, is_training, weight_decay, dropout):
+    with tf.contrib.slim.arg_scope(mobilenet_v2.training_scope(is_training=is_training,
+                                                               weight_decay=weight_decay)):
+        with tf.contrib.slim.arg_scope([tf.contrib.slim.conv2d],
+                                       normalizer_params={'scale': True, 'center': True, 'epsilon': 1e-5,
+                                                          'decay': 0.95}):
+            feature_map_size = tf.shape(net)
+            branch_1 = tf.reduce_mean(net, [1, 2], name='image_level_global_pool', keepdims=True)
+            branch_1 = tf.contrib.slim.conv2d(branch_1, 256, [1, 1], scope="image_level_conv_1x1",
+                                              activation_fn=tf.nn.relu6)
+            branch_1 = tf.image.resize_bilinear(branch_1, (feature_map_size[1], feature_map_size[2]),
+                                                align_corners=True)
+
+            branch_2 = tf.contrib.slim.conv2d(net, 256, [1, 1], scope='aspp0', activation_fn=tf.nn.relu6)
+
+            out = tf.concat([branch_1, branch_2], axis=-1)
+            out = tf.contrib.slim.conv2d(out, 256, [1, 1], scope='concat_projection', activation_fn=tf.nn.relu6)
+            out = tf.contrib.slim.dropout(out, keep_prob=1 - dropout, is_training=is_training)
+
+            out = tf.contrib.slim.conv2d(out, 2, [1, 1], scope='final_layer', normalizer_fn=None, activation_fn=None,
+                                         biases_initializer=tf.contrib.slim.initializers.xavier_initializer())
+            out = tf.image.resize_bilinear(out, (input_tensor.shape[1], input_tensor.shape[2]), align_corners=True)
+
+            return out
---
+++ my_dataset.py
@@ -0,0 +1,104 @@
+from collections import namedtuple
+import tensorflow as tf
+import augmentation
+
+Sample = namedtuple('Sample', ('image', 'polygon'))
+
+class MyDataset:
+    def __init__(self,
+                 filename,
+                 batch_size,
+                 shape,
+                 num_readers=1,
+                 num_classes=2,
+                 is_training=False,
+                 should_shuffle=False,
+                 should_repeat=False,
+                 should_augment=False):
+        self.filename = filename
+        self.num_classes = num_classes
+        self.shape = shape
+        self.batch_size = batch_size
+        self.num_readers = num_readers
+        self.is_training = is_training
+        self.should_shuffle = should_shuffle
+        self.should_repeat = should_repeat
+        self.should_augment = should_augment
+        self.ignore_label = 255
+
+    def _decode_image(self, content, channels):
+        return tf.cond(
+            tf.image.is_jpeg(content),
+            lambda: tf.image.decode_jpeg(content, channels),
+            lambda: tf.image.decode_png(content, channels))
+
+    def _parse_sample(self, sample):
+        features = {
+            'image':
+                tf.io.FixedLenFeature((), tf.string, default_value=''),
+            'height':
+                tf.io.FixedLenFeature((), tf.int64, default_value=0),
+            'width':
+                tf.io.FixedLenFeature((), tf.int64, default_value=0),
+            'mask':
+                tf.io.FixedLenFeature((), tf.string, default_value=''),
+            'name':
+                tf.io.FixedLenFeature((), tf.string, default_value=''),
+        }
+
+        parsed_features = tf.io.parse_single_example(sample, features)
+
+        sample = {
+            'image': self._decode_image(parsed_features['image'], 3),
+            'image_name': parsed_features['name'],
+            'height': parsed_features['height'],
+            'width': parsed_features['width'],
+            'label': self._decode_image(parsed_features['mask'], 1),
+        }
+
+        return sample
+    
+    def _preprocess_sample(self, sample):
+        preprocess = lambda x: (2.0 / 255.0) * tf.cast(x, tf.float32) - 1.0
+        sample['image'] = preprocess(sample['image'])
+        return [sample['image'], 
+                tf.one_hot(tf.reshape(sample['label'], shape=(self.shape[0], self.shape[1])), 
+                           depth=self.num_classes)]
+    
+    def _augment_sample(self, sample):
+        if self.should_augment:
+            sample['image'], sample['label'] = augmentation.flip_randomly_left_right_image_with_annotation(
+                sample['image'],
+                sample['label']
+            )
+            
+            # sample['image'] = tf.image.random_brightness(sample['image'], 0.1)
+
+            # sample['image'] = augmentation.distort_randomly_image_color(sample['image'])
+
+        return sample
+
+    def get(self):
+        """Gets an iterator that iterates across the dataset once.
+
+        Returns:
+          An iterator of type tf.data.Iterator.
+        """
+
+        dataset = (
+            tf.data.TFRecordDataset(self.filename)
+                .map(self._parse_sample, num_parallel_calls=self.num_readers)
+                .map(self._augment_sample, num_parallel_calls=self.num_readers)
+                .map(self._preprocess_sample, num_parallel_calls=self.num_readers)
+                )
+
+        if self.should_shuffle:
+            dataset = dataset.shuffle(buffer_size=100)
+
+        if self.should_repeat:
+            dataset = dataset.repeat()  # Repeat forever for training.
+        else:
+            dataset = dataset.repeat(1)
+
+        dataset = dataset.batch(self.batch_size).prefetch(self.batch_size)
+        return dataset
