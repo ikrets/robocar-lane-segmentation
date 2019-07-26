1f1dff757869ab7ddc8ca06338f43ff4405b1248
Ilia Krets <mrchair2170@gmail.com>
2019-07-26 13:07:47 +0200

    Set update moving to inplace

--- model.py
+++ model.py
@@ -7,8 +7,8 @@ def mobilenet_backbone(input_tensor, depth_multiplier,
     with tf.contrib.slim.arg_scope(mobilenet_v2.training_scope(is_training=is_training, weight_decay=weight_decay)):
         with tf.contrib.slim.arg_scope([tf.contrib.slim.conv2d],
                                        normalizer_params={'scale': True, 'center': True, 'epsilon': 1e-3,
-                                                          'fused': False,
-                                                          'decay': 0.95}):
+                                                          'decay': 0.95,
+                                                          'updates_collections': None}):
             logits, endpoints = mobilenet_v2.mobilenet(input_tensor=input_tensor,
                                                        num_classes=2,
                                                        depth_multiplier=depth_multiplier,
@@ -31,7 +31,8 @@ def segmentation_head(input_tensor, net, is_training, weight_decay, dropout):
         with tf.contrib.slim.arg_scope([tf.contrib.slim.conv2d],
                                        normalizer_params={'scale': True, 'center': True, 'epsilon': 1e-5,
                                                           'fused': False,
-                                                          'decay': 0.95}):
+                                                          'decay': 0.95,
+                                                          'updates_collections': None}):
             feature_map_size = tf.shape(net)
             branch_1 = tf.reduce_mean(net, [1, 2], name='image_level_global_pool', keepdims=True)
             branch_1 = tf.contrib.slim.conv2d(branch_1, 256, [1, 1], scope="image_level_conv_1x1",
